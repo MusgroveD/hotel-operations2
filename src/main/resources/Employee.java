5085e00c8d525cc94e4264d6ef2e51e175781acf
MusgroveD <musgrove.dru@gmail.com>
2026-05-05 10:15:28 -0400

    .

---
+++ src/main/resources/Employee.java
@@ -0,0 +1,74 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+    private int lastPunchInTime;
+
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+        this.lastPunchInTime = -1;
+    }
+
+    public void punchIn(int time) {
+        lastPunchInTime = time;
+    }
+
+    public void punchOut(int time) {
+        if (lastPunchInTime != -1) {
+            hoursWorked += time - lastPunchInTime;
+            lastPunchInTime = -1;
+        }
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getTotalPay() {
+        if (hoursWorked <= 40) {
+            return hoursWorked * payRate;
+        } else {
+            return (40 * payRate) + ((hoursWorked - 40) * (payRate * 1.5));
+        }
+    }
+
+    public double getRegularHours() {
+        if (hoursWorked <= 40) {
+            return hoursWorked;
+        } else {
+            return 40;
+        }
+    }
+
+    public double getOvertimeHours() {
+        if (hoursWorked > 40) {
+            return hoursWorked - 40;
+        } else {
+            return 0;
+        }
+    }
+}
